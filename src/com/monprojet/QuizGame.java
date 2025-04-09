3de31fb2390f9b6bf8b841aac6ed72f7f997f011
MaximeKELI <presidentnetero01@gmail.com>
2025-04-09 23:44:29 +0000

    JAVAGAME

---
+++ src/com/monprojet/QuizGame.java
@@ -0,0 +1,19 @@
+package com.monprojet;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.List;
+import java.io.*;
+import org.json.*;
+
+public class QuizGame {
+    private static final int SCREEN_WIDTH = 1024;
+    private static final int SCREEN_HEIGHT = 768;
+    // ... (ajoutez ici le reste de votre code)
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new QuizGame());
+    }
+}
